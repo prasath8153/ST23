ccdf7340c7c4b76df1c82365c3d4e761b6f31374
prasath8153 <informprasath@gmail.com>
2023-05-15 12:22:46 +0530

    hi

---
+++ src/org/AR/ArAd.java
@@ -0,0 +1,28 @@
+package org.AR;
+
+public class ArAd {
+public static void main(String[] args) {
+	//int a[]= {0,1,2,3,4,5,6,7,8,9,10};
+	//int sum=0;
+	//for (int i = 0; i < a.length; i++) {
+	//	sum+=i;
+	//}
+	//System.out.println("this sum is:"+sum);
+int [] num=new int[10];
+num[0]=1;
+num[1]=2;
+num[2]=3;
+num[3]=4;
+num[4]=5;
+num[5]=6;
+num[6]=7;
+num[7]=8;
+num[8]=9;
+num[9]=10;
+System.out.println(num[9]);
+int l=10;
+System.out.println(10);
+}
+
+}
+
